45e4ba7eac8f3359f96e5719e26138fc83133603
ufktncr <ufktncr@users.noreply.github.com>
2016-05-26 17:34:33 +0300

    v0.7.1

--- eftufuktuncer/views.py
+++ eftufuktuncer/views.py
@@ -1,9 +1,10 @@
 __author__ = 'ufuktuncer'
 
 from eftufuktuncer import app
-from flask import render_template, request, redirect, url_for, session
+from flask import render_template, request, redirect, url_for, session, Response
 from eftufuktuncer.models import *
 from .loginmanager import is_logged_in
+import json
 
 
 @app.route('/')
@@ -38,7 +39,7 @@ def register():
             error='Please fill all fields'
         else:
             try:
-                newuser = User(request.form['username'], request.form['password'], request.form['country'])
+                newuser = User(request.form['username'], request.form['password'], request.form.get('country'))
                 db.session.add(newuser)
                 db.session.commit()
                 return redirect(url_for('content'))
@@ -52,3 +53,23 @@ def register():
 @is_logged_in
 def content():
     return render_template('content.html')
+
+
+@app.route('/get_countries')
+def get_countries():
+    country_list = [
+        ('England', 'England'),
+        ('Germany', 'Germany'),
+        ('United States', 'United States')
+        ('Turkey', 'Turkey')
+    ]
+
+    countries = [{'value': i[0], 'name': i[1]} for i in country_list]
+
+    response_data = json.dumps(countries)
+
+    return Response(
+        response=response_data,
+        mimetype='application/json',
+        status=200
+    )
