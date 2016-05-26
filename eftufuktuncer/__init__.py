10c29d711bded875e9faab0f6c060cbfa2631e3f
ufktncr <ufktncr@users.noreply.github.com>
2016-05-26 14:12:26 +0300

    v0.4

--- eftufuktuncer/__init__.py
+++ eftufuktuncer/__init__.py
@@ -4,4 +4,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///eftufuktuncer.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 import eftufuktuncer.views
+import eftufuktuncer.models
---
+++ eftufuktuncer/models.py
@@ -0,0 +1,33 @@
+__author__ = 'ufuktuncer'
+
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash, generate_password_hash
+from eftufuktuncer import app
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    password_has = db.Column(db.String(200), unique=True)
+    country = db.Column(db.String(50), unique=False)
+
+    @property
+    def password(self):
+        raise AttributeError('password field is read-only')
+
+    @password.setter
+    def password(self, password):
+        self.password_has = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __init__(self, username, password, country):
+        self.username = username
+        self.password = password
+        self.country = country
+
+    def __repr__(self):
+        return self.username
