106700476417dadb41a126f158b187d7ddecc6f6
ufktncr <ufktncr@users.noreply.github.com>
2016-05-26 13:36:53 +0300

    v0.2

--- eftufuktuncer/views.py
+++ eftufuktuncer/views.py
@@ -1,8 +1,25 @@
 __author__ = 'ufuktuncer'
 
 from eftufuktuncer import app
+from flask import render_template
 
 
 @app.route('/')
 def welcome_page():
-    return 'Welcome to eftufuktuncer!'
+    return render_template('welcome.html'), 404
+
+
+@app.route('/login')
+def login():
+    error = None
+    return render_template('login.html', error=error)
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html'), 404
+
+
+@app.route('/content')
+def content():
+    return render_template('content.html')
